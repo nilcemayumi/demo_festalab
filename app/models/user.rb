500e2c9c4f9a9f9f26ca13bd2e7b58446ff25772
Nilce Moscone <nilcemayumi@gmail.com>
2023-10-07 10:14:33 -0300

    validação de email feito

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :name, :email, :phone, :cpf, presence: true
+  validates_format_of :email, with: EMAIL_REGEX
 end
