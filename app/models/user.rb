dd3a594d67548765328633d049f8697fd83ea552
Nilce Moscone <nilcemayumi@gmail.com>
2023-10-09 15:17:46 -0300

    Incluido gem e verificação para validar o cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  require 'cpf_cnpj'
+
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name, :email, :phone, :cpf, presence: true
   validates_format_of :email, with: EMAIL_REGEX
+  validates :cpf, uniqueness: { message: 'CPF já cadastrado' }
+  validate :validate_cpf
+
+  def validate_cpf
+    errors.add(:cpf, 'CPF inválido') unless CPF.valid?(cpf)
+  end
 end
